cb2bab360de846c1346628ce9ca92c8e38fc4018
OSPITE <OSPITE@Info1-PC3>
2020-10-14 09:44:17 +0200

    esercizio metodi switch

---
+++ src/switchMain.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class switchMain {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int [][] matrix1, matrix2;
+		int cell1, cell2;
+		
+		System.out.println("Inserisci il numero di righe della prima matrice:");
+		cell1 = sc.nextInt();
+		System.out.println("Inserisci il numero di righe della seconda matrice:");
+		cell2 = sc.nextInt();
+		
+		matrix1 = new int [cell1][cell1];
+		matrix2 = new int [cell2][cell2];
+		
+		
+
+	}
+
+}
