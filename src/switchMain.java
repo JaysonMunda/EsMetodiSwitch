21596761efef75410e9ad44bebf52cd7056e01b6
OSPITE <OSPITE@Info1-PC3>
2020-10-19 09:44:18 +0200

    new

--- src/switchMain.java
+++ src/switchMain.java
@@ -2,6 +2,31 @@ import java.util.Scanner;
 
 public class switchMain {
 	
+	public static void calcMinMax(int m[][], int v[][]) {
+		Scanner sc = new Scanner(System.in);
+		int r1, r2;
+		int min=0, max=0;
+		
+		System.out.println("Quale riga vuoi nella prima matrice?");
+		r1 = sc.nextInt();
+		System.out.println("Quale riga vuoi nella seconda matrice?");
+		r2 = sc.nextInt();
+		
+		m = new int[r1][];
+		v = new int[r2][];
+		
+		for(int i=0; i<m[r1].length; i++) {
+			if(m[r1][i] < min) {
+				min = m[r1][i];
+			}
+			if(m[r1][i] > max) {
+				max = m[r1][i];
+			}
+		}
+		System.out.println(min);
+		System.out.println(max);
+	}
+	
 	public static void carVisMat(int m[][]) {
 		for(int i=0; i<m.length; i++) {
 			for(int j=0; j<m[i].length; j++) {
@@ -27,11 +52,14 @@ public class switchMain {
 		carVisMat(matrix1);
 		carVisMat(matrix2);
 		
-		System.out.println("Inserisci un numero tra 1 e 4");
-		n = sc.nextInt();
+		do {
+			System.out.println("Inserisci un numero tra 1 e 4");
+			n = sc.nextInt();
+		} while (n<1 || n>4);
 		
 		switch(n) {
 		case 1:
+			calcMinMax(matrix1, matrix2);
 			break;
 		case 2:
 			break;
@@ -40,9 +68,10 @@ public class switchMain {
 		case 4:
 			break;
 		default:
-			System.err.println("Inserire un numero tra 1 e 4");
+			//System.err.println("Inserire un numero tra 1 e 4");
 			break;
 		}
+		
 
 	}
 
